4824cd13e38ff4114ac16890ae2a6548b16f2536
ALINE BERNARDI DA ROCHA <ALU201530615@uniritter.local>
2016-06-10 22:11:46 -0300

    Criação do model orders

---
+++ UniRitter.UniRitter2016/Models/Orders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniRitter.UniRitter2016.Models
+{
+      public enum OrderStatus
+    {
+        new_,
+        processed,
+        invoiced,
+        delivered,
+        canceled
+    }
+    public class OrderItens
+    {
+     Guid? product_id { get; set; }
+     int quantity { get; set; }
+    decimal price { get; set; }
+
+    }
+    class Orders
+    {
+        Guid? id { get; set; }
+        public OrderStatus status { get; set; }
+        public decimal total { get; set; }
+        public DateTime created_on { get; set; }
+        public DateTime updated_on { get; set; }
+        public  List<OrderItens> itens { get; set; }
+
+    }
+}
+
+
+
+
+
+
